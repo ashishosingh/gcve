47944ad5d412d222a7285b4f3be110037978bdcc
ashishosingh <109507677+ashishosingh@users.noreply.github.com>
2025-07-25 16:31:17 +0000

    initial commit

---
+++ powercli/datastore/copy_remote_local.ps1
@@ -0,0 +1,5 @@
+
+$datastore = Get-Datastore -Name "vsanDatastore"
+$sourcePath = $datastore.ExtensionData.Info.Name + ":/VMNAME/VMNAME.vmdk"
+$destinationPath = "C:\savehere"
+Copy-DatastoreItem -Item $sourcePath -Destination $destinationPath
